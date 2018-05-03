a1d781317eb6ea9f383eafca37a778a12ae5612f
unknown <acevedo1500@gmail.com>
2018-05-03 18:56:40 -0500

    cambio headers

--- seguimiento2/MiEmpleadoServer/server.js
+++ seguimiento2/MiEmpleadoServer/server.js
@@ -106,14 +106,10 @@ empleadoRoute.delete(function(req, res) {
 
 app.use('/api', router);
 app.use((req,res,next) => {
-  //Con lo siguiente permitimos acceso a todos los dominios.
-  res.header('Access-Control-Allow-Origin' , '*');
-  //Para que funcione a nivel de Ajax
-  res.header('Access-Control-Allow-Headers' , 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method  ');
-  //Indicamos los metodos que se van a soportar
-  res.header('Access-Control-Allow-Methods' , 'GET, POST, OPTIONS, PUT, DELETE');
-
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin:*");
+  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
+  res.header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With, X-CLIENT-ID, X-CLIENT-SECRET');
+  res.header('Access-Control-Allow-Credentials: true');
   //Salir del middleware y continuar con el flujo normal de ejecución, con una ruta concreta de un metodo de un controaldor
   next();
 });
