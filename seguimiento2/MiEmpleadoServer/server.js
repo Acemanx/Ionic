03128fa7d43e4161120807bf7980033c137b00cf
unknown <acevedo1500@gmail.com>
2018-05-03 20:34:41 -0500

    add

--- seguimiento2/MiEmpleadoServer/server.js
+++ seguimiento2/MiEmpleadoServer/server.js
@@ -105,24 +105,18 @@ empleadoRoute.delete(function(req, res) {
 });
 
 app.use('/api', router);
-app.use(function (req, res, next) {
+app.use((req,res,next) => {
+  //Con lo siguiente permitimos acceso a todos los dominios.
+  res.header('Access-Control-Allow-Origin' , '*');
+  //Para que funcione a nivel de Ajax
+  res.header('Access-Control-Allow-Headers' , 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method  ');
+  //Indicamos los metodos que se van a soportar
+  res.header('Access-Control-Allow-Methods' , 'GET, POST, OPTIONS, PUT, DELETE');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-  });
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  //Salir del middleware y continuar con el flujo normal de ejecución, con una ruta concreta de un metodo de un controaldor
+  next();
+});
 
 // Start the server
 app.listen(port);
